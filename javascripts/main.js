25805b8b7ab406d0a2601d85996e95ef8e83e07b
Sliqric7053 <isloan@ISLOAN.local>
2016-08-02 11:09:26 +0200

    Update main.js
    Add test javascript data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("textarea")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
+
+$('textarea').addClass("richard")
